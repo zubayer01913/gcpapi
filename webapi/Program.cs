866e1e78a6ffe44029c7900b34c2a213d629a6c1
Zubayer Bin Ayub <zubayer01913@gmail.com>
2023-12-17 23:35:21 +0600

    update program

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -16,25 +16,11 @@ namespace webapi
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
-
-            if (environment == "Development")
-            {
-                return Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-            }
-            else
-            {
-                var url = new[]
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    string.Concat("http://0.0.0.0:", port)
-                };
-                return Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>().UseUrls(url); });
-            }
-        }
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
